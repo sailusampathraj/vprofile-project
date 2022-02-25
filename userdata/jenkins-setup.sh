d508b61e5b46e5cabda56e1243ce272e883185e4
sailusampathraj <86949543+sailusampathraj@users.noreply.github.com>
2022-02-25 13:03:50 +0530

    Update jenkins-setup.sh
    Java version changed to jdk11

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
+sudo apt install openjdk-11-jdk -y
 sudo apt install ca-certificates -y
 sudo apt install maven git wget unzip -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
