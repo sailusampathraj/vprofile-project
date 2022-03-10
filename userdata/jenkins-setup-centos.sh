de7875c4f5dcba216812e1f5f042e604303ea332
sailusampathraj <86949543+sailusampathraj@users.noreply.github.com>
2022-03-10 17:20:30 +0530

    Update jenkins-setup-centos.sh

--- userdata/jenkins-setup-centos.sh
+++ userdata/jenkins-setup-centos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
 sudo yum install java-11-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
